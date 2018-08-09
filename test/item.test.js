e9d20d3a3419cb15d205006ca37a2834045e28ff
Predrag Stojadinovic <predrags@nvidia.com>
2018-08-09 16:44:43 +0200

    wip on player and middle item classes

---
+++ test/item.test.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+const expect = require("chai").expect;
+
+const PrefPaperItem = require("../lib/item");
+
+describe("PrefPaperItem tests", function () {
+	it("PrefPaperItem should exist", function () {
+		expect(PrefPaperItem).to.exist;
+	});
+});
