2234bbb5f62a36dbe1f199466e72f185ffd5fef3
Predrag Stojadinovic <predrags@nvidia.com>
2018-08-09 15:07:22 +0200

    wip on middle and side items classes

---
+++ test/middle-item.test.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+const expect = require("chai").expect;
+
+const PrefPaperMiddleItem = require("../lib/middle-item");
+
+describe("PrefPaperMiddleItem tests", function () {
+	it("PrefPaperMiddleItem should exist", function () {
+		expect(PrefPaperMiddleItem).to.exist;
+	});
+});
---
+++ test/side-item.test.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+const expect = require("chai").expect;
+
+const PrefPaperSideItem = require("../lib/side-item");
+
+describe("PrefPaperSideItem tests", function () {
+	it("PrefPaperSideItem should exist", function () {
+		expect(PrefPaperSideItem).to.exist;
+	});
+});
