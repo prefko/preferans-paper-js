537a81fabd074eadd84935f72471a307c151751c
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-08-11 20:23:24 +0200

    getting closer to completion

--- run.js
+++ run.js
@@ -5,14 +5,4 @@ const _ = require("lodash");
 const PrefPaper = require("./lib/paper");
 const PrefPaperPlayer = require("./lib/player");
 
-let p = new PrefPaper(60, 3, {
-	unlimitedRefe: false,
-	playPikOnRefa: false,
-	lastHandDoubleFall: false,
-	lastHandLimitSoup: false,
-	failPikAfterRefas: false,
-	failPikAfterOneUnderZero: false,
-	allowSubAndMortKontras: false
-});
-
 // console.log(p);
