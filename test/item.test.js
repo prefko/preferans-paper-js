66ccd0b45664bcfc2e759047426f555b5ccf2b90
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-08-12 12:42:12 +0200

    fixing codacy issues

--- test/item.test.js
+++ test/item.test.js
@@ -8,8 +8,8 @@ describe("PrefPaperItem tests", function () {
 		expect(PrefPaperItem).to.exist;
 	});
 
-	describe('PrefPaperItem constructor tests', function () {
-		it('contructor should create object', function () {
+	describe("PrefPaperItem constructor tests", function () {
+		it("contructor should create object", function () {
 			expect(() => new PrefPaperItem()).to.throw();
 			expect(() => new PrefPaperItem(15)).to.throw();
 			expect(() => new PrefPaperItem(-20)).to.throw();
@@ -17,30 +17,30 @@ describe("PrefPaperItem tests", function () {
 			expect(() => new PrefPaperItem(60)).to.not.throw();
 			expect(() => new PrefPaperItem(60, false)).to.not.throw();
 			expect(() => new PrefPaperItem(60, true)).to.not.throw();
-			expect(new PrefPaperItem(80)).to.be.an('object');
-			expect(new PrefPaperItem(80, false)).to.be.an('object');
-			expect(new PrefPaperItem(80, true)).to.be.an('object');
+			expect(new PrefPaperItem(80)).to.be.an("object");
+			expect(new PrefPaperItem(80, false)).to.be.an("object");
+			expect(new PrefPaperItem(80, true)).to.be.an("object");
 		});
 	});
 
-	describe('PrefPaperItem isInvalidated tests', function () {
-		it('isInvalidated should return proper value', function () {
+	describe("PrefPaperItem isInvalidated tests", function () {
+		it("isInvalidated should return proper value", function () {
 			expect(new PrefPaperItem(80).isInvalidated()).to.be.false;
 			expect(new PrefPaperItem(80, false).isInvalidated()).to.be.false;
 			expect(new PrefPaperItem(80, true).isInvalidated()).to.be.true;
 		});
 	});
 
-	describe('PrefPaperItem isRefa tests', function () {
-		it('isRefa should always return false', function () {
+	describe("PrefPaperItem isRefa tests", function () {
+		it("isRefa should always return false", function () {
 			expect(new PrefPaperItem(80).isRefa()).to.be.false;
 			expect(new PrefPaperItem(80, false).isRefa()).to.be.false;
 			expect(new PrefPaperItem(80, true).isRefa()).to.be.false;
 		});
 	});
 
-	describe('PrefPaperItem getValue tests', function () {
-		it('getValue should proper value', function () {
+	describe("PrefPaperItem getValue tests", function () {
+		it("getValue should proper value", function () {
 			expect(new PrefPaperItem(80).getValue()).to.be.equal(80);
 			expect(new PrefPaperItem(80, false).getValue()).to.be.equal(80);
 			expect(new PrefPaperItem(80, true).getValue()).to.deep.equal({invalidated: true, value: 80});
