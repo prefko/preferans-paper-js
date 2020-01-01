361ecfda854e262d5d4f58c1507557a932c4532b
predrags <predrags@nvidia.com>
2020-01-01 21:24:39 +0100

    wip

--- src/prefPaperColumnSide.ts
+++ src/prefPaperColumnSide.ts
@@ -13,7 +13,18 @@ export default class PrefPaperColumnSide extends PrefPaperColumn {
 		this._position = position;
 	}
 
-	private pushValue(value: number, repealed: boolean = false): PrefPaperEntryNumber {
+	public addValue(value: number): PrefPaperColumn {
+		const entry = this._pushValue(value);
+		this._value = entry.value;
+		return this;
+	}
+
+	public addValueRepealed(value: number): PrefPaperColumn {
+		this._pushValue(value, true);
+		return this;
+	}
+
+	private _pushValue(value: number, repealed: boolean = false): PrefPaperEntryNumber {
 		if (!PrefPaperColumn.isValidValue(value)) throw new Error("PrefPaperColumn::pushValue:Value is not valid: " + value + ". Value must be larger than 0 and even.");
 
 		const newValue = this._value + value;
@@ -22,17 +33,6 @@ export default class PrefPaperColumnSide extends PrefPaperColumn {
 		this._values.push(entry);
 
 		return entry;
-	}
-
-	public addValue(value: number): PrefPaperColumn {
-		const entry = this.pushValue(value);
-		this._value = entry.value;
-		return this;
-	}
-
-	public addValueRepealed(value: number): PrefPaperColumn {
-		this.pushValue(value, true);
-		return this;
 	}
 
 }
